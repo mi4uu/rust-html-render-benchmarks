6b574b3121517ce016ae28cf09cc2d9e434ce5fd
mi4uu <michal.lipinski@gmail.com>
2025-02-24 04:55:36 +0000

    nn

---
+++ run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# List of features (benchmarks) to iterate over
+FEATURES=(
+    "hyped"
+    "maud"
+    "askama"
+    "tera"
+    "tinytemplate"
+    "handlebars"
+    "minijinja"
+    "html_node"
+    "hypertext"
+    "vy"
+    "fhtml"
+)
+
+# Iterate over each feature
+for FEATURE_NAME in "${FEATURES[@]}"; do
+    echo "Running benchmarks for feature: $FEATURE_NAME"
+
+    # Clean the project
+    echo "Running cargo clean..."
+    cargo clean
+
+    # Run benchmarks with the current feature
+    echo "Running cargo bench for $FEATURE_NAME..."
+    cargo bench -Z unstable-options --timings=html --features "$FEATURE_NAME"
+
+    mv target/cargo-timings/cargo-timing.html "docs/cargo-timing-${FEATURE_NAME}.html"
+    echo "<hr><a href=\"cargo-timing-${FEATURE_NAME}.html\"" >> docs/list.html"
+
+    # Check if the command succeeded
+    if [ $? -eq 0 ]; then
+        echo "Benchmarks for $FEATURE_NAME completed successfully."
+    else
+        echo "Benchmarks for $FEATURE_NAME failed."
+        exit 1
+    fi
+
+    echo "----------------------------------------"
+done
+
+echo "All benchmarks completed."
